f5203468a268a1f11514ca692cfba0996ddba736
riranrmarques-ctrl <riran.rmarques@gmail.com>
2026-06-10 17:17:31 -0300

    Create noticias_pctsul.js

---
+++ noticias_pctsul.js
@@ -0,0 +1,20 @@
+const news = [
+  {
+    title: "Pesquisadores criam referência inédita para o cacau do Sul da Bahia",
+    date: "4 de jun. de 2026",
+    summary: "Estudo liderado pelo PCTSul definiu faixas de micronutrientes para lavouras de cacau, ajudando produtores a tomar decisões mais precisas sobre adubação.",
+    image: "https://framerusercontent.com/images/k5NInxmqyZQKQ5G3zVT3Cwg.png?width=2048&height=1365"
+  },
+  {
+    title: "Sul da Bahia amplia oferta de qualificação para a cadeia do cacau",
+    date: "23 de abr. de 2026",
+    summary: "O CIC lançou sua programação de cursos para 2026, fortalecendo a formação em cacau especial e chocolate bean to bar.",
+    image: "https://framerusercontent.com/images/dFFwG6ouF0OE3jc54nXsxdv5AYw.png?width=1920&height=910"
+  },
+  {
+    title: "Inscrições abertas para o Concurso Nacional de Cacau Especial do Brasil",
+    date: "19 de mar. de 2026",
+    summary: "A 8ª edição reconhece os melhores lotes do país e integra a seletiva brasileira para uma das principais premiações internacionais do setor.",
+    image: "https://framerusercontent.com/images/KFjS88Xj42nytSuiD5MVYyIZgo.png?width=2048&height=933"
+  }
+];
